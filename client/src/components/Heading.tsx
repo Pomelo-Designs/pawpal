084a27346774dc0fdd8b30c739753736940f1351
rockbrok <glennp167@gmail.com>
2023-05-06 14:42:50 -0300

    Updated adoption

--- client/src/components/Heading.tsx
+++ client/src/components/Heading.tsx
@@ -1,3 +1,7 @@
 export const Heading = ({ label }: any) => (
   <h3 className="font-karla whitespace-pre-line text-4xl p-12 pl-0">{label}</h3>
 );
+
+export const SubHeading = ({label}: any) => (
+  <h4 className="text-xl text-[22px] text-[#201A18] p-2.5 pl-0">{label}</h4>
+);
--- client/src/routes/Pets/Adoption.tsx
+++ client/src/routes/Pets/Adoption.tsx
@@ -1,18 +1,18 @@
-import { Heading } from "../../components/Heading";
-import { useQuery } from "@apollo/client";
-import { QUERY_ALL_ADOPTIONS } from "../../queries/Adoption";
+import { Heading, SubHeading } from "../../components/Heading";
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_ALL_ADOPTIONS, MUTATE_ADOPTION_BY_ID } from "../../queries/Adoption";
 import { useState } from "react";
 
 export default function Adoption() {
   const [input, setInput] = useState({
     // "offset": 0,
     // "limit": 3,
-    "gender": null,
-    "species": null,
-    "livedWith": null,
-    "age": null,
-    "sortByAge": null,
-    "sortBySize": "DESC"
+    gender: null,
+    species: null,
+    livedWith: null,
+    age: null,
+    sortByAge: null,
+    sortBySize: "DESC"
   })
 
   const handleInput = (newInput: any) => {
@@ -27,6 +27,8 @@ export default function Adoption() {
     variables: { input }
   });
 
+
+
   if (loading) return <h1>Data loading...</h1>;
   if (error) console.log(error);
   if (data) console.log(data);
@@ -37,24 +39,78 @@ export default function Adoption() {
         <Heading label={`Available pets\nfor adoption`} />
 
 
-        <span>Gender</span>
+        <SubHeading label={`By gender`} />
         <div className="flex flex-row gap-4">
-          <button onClick={() => handleInput({ gender: null })}>All</button>
-          <button onClick={() => handleInput({ gender: "male" })}>Male</button>
-          <button onClick={() => handleInput({ gender: "female" })}>Female</button>
+          {/* <button onClick={() => handleInput({ gender: null })}>All</button>
+          <button onClick={() => handleInput({ gender: "m" })}>Male</button>
+          <button onClick={() => handleInput({ gender: "f" })}>Female</button> */}
+          <Chip
+            click={() => handleInput({ gender: null })}
+            label="All"
+            input={input.gender}
+          />
+          <Chip
+            click={() => handleInput({ gender: "m" })}
+            label="Male"
+            input={input.gender}
+          />
+            <Chip
+            click={() => handleInput({ gender: "f" })}
+            label="Female"
+            input={input.gender}
+          />
         </div>
 
-        <span>Species</span>
-        <div className="flex flex-row gap-4">
-          <button onClick={() => handleInput({ species: null })}>All</button>
-          <button onClick={() => handleInput({ species: "dog" })}>Dog</button>
-          <button onClick={() => handleInput({ species: "cat" })}>Cat</button>
-          <button onClick={() => handleInput({ species: "critter" })}>Critter</button>
-          <button onClick={() => handleInput({ species: "bird" })}>Bird</button>
+        <SubHeading label={`Lived with`} />
+        <div className="flex flex-row gap-6">
+          <Chip
+            click={() => handleInput({ livedWith: null })}
+            label="All"
+            input={input.livedWith}
+          />
+           <Chip
+            click={() => handleInput({ livedWith: "children" })}
+            label="Children"
+            input={input.livedWith}
+          />
+           <Chip
+            click={() => handleInput({ livedWith: "animals" })}
+            label="Animals"
+            input={input.livedWith}
+          />
+        </div>
+
+        <SubHeading label={`By species`} />
+        <div className="flex flex-row gap-6">
+          <Chip
+            click={() => handleInput({ species: null })}
+            label="All"
+            input={input.species}
+          />
+          <Chip
+            click={() => handleInput({ species: "dog" })}
+            label="Dogs"
+            input={input.species}
+          />
+          <Chip
+            click={() => handleInput({ species: "cat" })}
+            label="Cats"
+            input={input.species}
+          />
+          <Chip
+            click={() => handleInput({ species: "critter" })}
+            label="Critters"
+            input={input.species}
+          />
+          <Chip
+            click={() => handleInput({ species: "bird" })}
+            label="Birds"
+            input={input.species}
+          />
         </div>
 
 
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-3 gap-6">
           {data &&
             data.adoptions.map((animal: any) => {
               console.log('animal : ', animal);
@@ -62,6 +118,7 @@ export default function Adoption() {
                 <Card
                   data={animal}
                   key={animal.id}
+                  handleInput={handleInput}
                 />
               )
             })}
@@ -72,17 +129,69 @@ export default function Adoption() {
   );
 }
 
+const Chip = ({ click, label, input }: any) => {
+
+  const style = () => {
+    if (input === null && label === "All") return "bg-[#FFDBCC] border-none";
+    else if (label.toLowerCase().includes(input)) return "bg-[#FFDBCC] border-none";
+    else return null;
+  }
+
+  return (
+    <button
+      onClick={click}
+      className={`${style()} flex items-center justify-center h-8 w-[72px] border-[#857371] border-[1px] rounded-xl`}>
+      <span className="text-xs color-[#534342] font-medium">{label}</span>
+    </button>
+  )
+}
+
 const Card = ({ data }: any) => {
   return (
-    <div className="flex flex-col h-64 w-64 rounded-xl bg-pink-300">
+    <div className="flex flex-col h-[272px] w-full rounded-xl bg-pink-300">
       <div className="h-[72px] flex flex-row justify-between">
         <div className="pt-2.5 pl-4">
           <h4 className="text-xl">{data.name}</h4>
           <span className="text-sm font-medium color-[#534342]">{data.age} years</span>
         </div>
-        <span className="material-symbols-rounded self-center pr-4"
-          style={{ fontSize: "32px", color: "#CE4374" }}>favorite</span>
+        <Like
+          data={data}
+        />
       </div>
+      <img
+        className="rounded-b-xl"
+        style={{ height: "200px", width: "100%" }} src={data.data} />
     </div>
+  )
+}
+
+const Like = ({ data, handleInput }: any) => {
+  const [liked, setLiked] = useState(data.liked);
+  const [mutateFunction, { loading, error }] = useMutation(MUTATE_ADOPTION_BY_ID);
+
+  const variables = {
+    id: data.id,
+    liked: liked,
+  }
+
+  const handleClick = () => {
+    setLiked(!liked);
+    mutateFunction({ variables: { variables } });
+  };
+
+  if (error) console.log(error);
+
+  console.log(data.liked)
+
+  const iconStyle = () => {
+    if (liked === true) return "material-symbols-rounded material-active";
+    else return "material-symbols-rounded";
+  }
+
+  return (
+    <button onClick={handleClick}>
+      <span className={`${iconStyle()} self-center pr-4`}
+        style={{ fontSize: "32px", color: "#CE4374" }}>favorite</span>
+    </button>
   )
 }
